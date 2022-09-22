ea826e2cd9d29d6132b734c3c0be95a8cb25970d
Noah Onder de Linden <95483517+Rackmoer@users.noreply.github.com>
2022-09-22 08:55:30 +0000

    gamestate werkt nog niet, alleen beginscherm

--- script.js
+++ script.js
@@ -1,59 +1,38 @@
 
-// var gameState = 0;
-// function draw() {
+let gameState = 0;
+function setup() {
+  createCanvas(800, 400);
+}
+function keyPressed() {
 
-//   text("gameState" + gameState, 25, 25);
+  if (keyCode == 49) {
+    gameState += 1;
+  console.log(gameState);
 
-//   if (gameState == 0) {
-//     menu();
-//   }
+  } else if(gameState == 2){
+    gameState = 0;
+  }
 
-//   if (gameState == 1) {
-//     game();1
-//   }
+}
+function draw() {
+  background(255);
 
-//   if (gameState == 2) {
-//     gameOver();
-//   }
-// }
+  if(gameState == 0){
+    startGame();
+  } else if(gameState == 1){
+    playGame();
+}
 
-// var x = 0;
+function startGame(){
+  background("green");
+  textAlign(CENTER);
+  textSize(20);
+  text("PRESS 1 TO START GAME", width/2,height/2);
 
-// function menu() {
-//   background("#ababab");
-//   text("MENU", 25, 45);
-//   text("1. menu", 25, 65);
-//   text("2. start game", 25, 85);
-//   text("3. game over", 25, 105);
-// }
+}
 
-// function game() {
-//   background("yellow");
-//   text("GAME RUNNING", 25, 45);
-//   // draw balls etc...
-//   fill("red");
-//   circle(x, 10, 10);
-//   x += 10;
-//   if(x > 500){
-//     gameState = 2;
-//   }
-// }
-
-// function keyPressed() {
-
-//   if (keyCode == 49) {
-//     gameState = 0;
-//   }
-
-//   if (keyCode == 50) {
-//     gameState = 1;
-//   }
-
-//   if (keyCode == 51) {
-//     gameState = 2;
-//   }
-// }
-
+function playGame(){
+ 
 class Bal{
     constructor(x,y,w,h,vx,vy,c){
     this.x = x;
@@ -82,63 +61,57 @@ class Bal{
    
     if (this.y > 400 || this.y < 0){
     this.vy = this.vy * -1; 
-      
     }
  }
-}
-
-
-var bal1, bal2, bal3, bal4;
-var lineX, lineY;
-var blok1; 
-
-function setup(){
-  createCanvas(800, 400);
-  bal1 = new Bal(400,200,25,25, 0, 0,'white');
-}
-
-
-function draw() {
-  background("lightblue");
-  bal1.draw();
-  if(lineX){
-    line(lineX, lineY, mouseX, mouseY);	
+ }
+  function setup(){
+    bal1 = new Bal(400,200,25,25, 0, 0,'white');
   }
-}
-
-function mousePressed() {
-  if (bal1.vx > 0.005 || bal1.vy > 0.005){
-   lineX = 0 
-   lineY = 0
-   }
-  else {
-   lineX = bal1.x;
-   lineY = bal1.y;
+  
+  function draw() {
+    background("lightblue");
+    bal1.draw();
+    if(lineX){
+      line(lineX, lineY, mouseX, mouseY);	
+    }
   }
-}
-
-function mouseReleased() {
-lineX = 0
-lineY = 0  
-
- 
-}
-
-function mouseClicked(){
-  if (bal1.vx > 0.005 || bal1.vy > 0.005){
-    vx = 0
-    lineX = 0 
-    lineY = 0
+  
+  function mousePressed() {
+    if (bal1.vx > 0.005 || bal1.vy > 0.005){
+     lineX = 0 
+     lineY = 0
+     }
+    else {
+     lineX = bal1.x;
+     lineY = bal1.y;
+    }
   }
-  else{
-    let vx = 0
-    let distX = mouseX - bal1.x;
-    let distY = bal1.y - mouseY;
-    let timeX = distX / vx;
-    let speedY = distY / timeX;
-    let vy = speedY;
-    bal1.vx = distX / 10;
-    bal1.vy = distY / 10 * -1;
+  
+  function mouseReleased() {
+  lineX = 0
+  lineY = 0  
+  
+   
   }
-}
+  
+  function mouseClicked(){
+    if (bal1.vx > 0.005 || bal1.vy > 0.005){
+      vx = 0
+      lineX = 0 
+      lineY = 0
+    }
+    else{
+      let vx = 0
+      let distX = mouseX - bal1.x;
+      let distY = bal1.y - mouseY;
+      let timeX = distX / vx;
+      let speedY = distY / timeX;
+      let vy = speedY;
+      bal1.vx = distX / 10;
+      bal1.vy = distY / 10 * -1;
+    }
+  }
 
+
+}
+}
