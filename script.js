914d305606241ea112c3a8c3c114adc7c0305ae8
Hugo Luijkx <95089154+HugoLuijkx@users.noreply.github.com>
2022-09-28 12:40:08 +0000

    bal class eigen file

--- script.js
+++ script.js
@@ -22,38 +22,7 @@ function startGame(){
 
 
    
-  class Bal{
-      constructor(x,y,w,h,vx,vy,c){
-      this.x = x;
-      this.y = y;
-      this.width = w;
-      this.height = h;
-      this.vx = vx;
-      this.vy = vy;
-      this.color = c;
-      this.gravity = 1.05
-      }
-      
-    
-      draw(){
-      fill(this.color)
-      ellipse(this.x,this.y,this.width,this.height)
-      this.vx /= this.gravity
-      this.vy /= this.gravity
-      this.x = this.x + this.vx;
-      this.y = this.y + this.vy;
-      
-      //collision
-      if (this.x > 795 || this.x < 25){
-      this.vx = this.vx * -1;
-      }
-     
-      if (this.y > 400 || this.y < 0){
-      this.vy = this.vy * -1; 
-        
-      }
-   }
-  }
+
   
   
   var bal1, bal2, bal3, bal4;
